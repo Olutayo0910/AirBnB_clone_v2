14eaa4a4ed1d045da9f13e31022165e9189e8f9e
Nkemjika1divine <cndivine@gmail.com>
2023-11-20 09:20:48 -0800

    User fix

--- models/user.py
+++ models/user.py
@@ -2,12 +2,22 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from os import getenv
+
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
