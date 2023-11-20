ac4c9140b38eb26d0a093501e596ef9601fc75f1
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-20 06:26:24 -0800

    corrected typo on performance

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
--- This scriot creates a database in preparation fir the project
+-- This scriot creates a database and user
+-- Sets password for the user
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
