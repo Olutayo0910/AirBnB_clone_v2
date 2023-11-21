eb1a7e8733a418933574e8b2074451f642068e86
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-21 12:47:18 -0800

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models
+import models
 from sqlalchemy import Column, DateTime, String, Integer, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
