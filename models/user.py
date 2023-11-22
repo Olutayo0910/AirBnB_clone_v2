e791b1c2af1b11e6371d162b2624340f7a4f2a57
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-22 03:23:46 -0800

    updated user

--- models/user.py
+++ models/user.py
@@ -2,12 +2,27 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String, ForeignKey
-from os import getenv
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+
+    places = relationship(
+        "Place",
+        cascade="all,delete",
+        backref=backref("user", cascade="all,delete"),
+        passive_deletes=True,
+        single_parent=True)
+
+    reviews = relationship(
+        "Review",
+        cascade="all,delete",
+        backref=backref("user", cascade="all,delete"),
+        passive_deletes=True,
+        single_parent=True)
