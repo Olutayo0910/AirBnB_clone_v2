fccc1f8f5af345e41b0efd755ec7f7c25a295d95
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-22 04:02:07 -0800

    updated place

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 
