d1bc78b92f7dce4d0670b9ca1bb2e3fbb56ff2f4
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-22 03:28:39 -0800

    correctd user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
 
