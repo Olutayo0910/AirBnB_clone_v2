309bfacac8d703d263e754e9abcb3c37e7a3d554
Nkemjika1divine <cndivine@gmail.com>
2023-11-22 07:49:17 -0800

    place fix

--- models/city.py
+++ models/city.py
@@ -3,17 +3,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
+from os import getenv
+
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60),
-                      ForeignKey("states.id", ondelete="CASCADE"),
-                      nullable=False)
-    places = relationship(
-        "Place",
-        cascade="all",
-        backref=backref("cities", cascade="all"),
-        passive_deletes=True)
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", cascade="all,delete", backref="cities")
+    else:
+        state_id = ""
+        name = ""
--- models/place.py
+++ models/place.py
@@ -6,19 +6,32 @@ from sqlalchemy.orm import relationship, backref
 from os import getenv
 
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id",ondelete="CASCADE"),
-                     nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id",ondelete="CASCADE"),
-                     nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
+    if storage_type == "db":
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
