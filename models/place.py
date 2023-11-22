7575688becc8a4565740648c9f0823ff576d6e52
Nkemjika1divine <cndivine@gmail.com>
2023-11-22 15:25:56 -0800

    amenity fix

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, Table, ForeignKey, Float
 from sqlalchemy.orm import relationship, backref
 from os import getenv
 
