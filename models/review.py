6084bfe007109900968718f08fa529b5ce4203ff
Nkemjika1divine <cndivine@gmail.com>
2023-11-22 09:11:57 -0800

    review fix

--- models/review.py
+++ models/review.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     if storage_type == "db":
