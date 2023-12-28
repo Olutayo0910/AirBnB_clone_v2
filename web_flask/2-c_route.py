1f1c4c18e6eb88153784b2e3abb7cc171b99db57
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-12-27 17:31:48 -0800

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Display “C ” followed by the value of the text variable"""
-    return 'C {}'.format(escape(text).replace('_', ' '))
+    return 'C {}'.format(text.replace('_', ' '))
     
                 
 
