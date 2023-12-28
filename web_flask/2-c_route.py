7aff2e10ab66151e93beaf647f09a87404de6fd9
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-12-27 17:41:24 -0800

    task 2 updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -13,16 +13,18 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
+
 @app.route('/', strict_slashes=False)
 def hbnb():
     """Displays HBNB"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Display “C ” followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
-    
-                
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
