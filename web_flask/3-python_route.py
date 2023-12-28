3d83d79f50f484641d82e6f21982234ef7f329c4
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-12-28 05:05:32 -0800

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c_text(text):
 @app.route('/python', strict_slashes=False)
 def c_python(text="is cool"):
     """Display “Python ”, followed by the value of the text"""
-    return 'Python {}'.format(text.replace('_', ' ')
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == "__main__":
